10c99227669856ba10f542aedff998d44022d758
Pretcb-Air <director@druk.kh.ua>
2024-01-03 23:04:12 +0200

    changed to async await

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,16 +5,27 @@ import { useState } from 'react';
 const API_URL = 'https://jsonplaceholder.typicode.com/posts/';
 
 function Posts() {
-	const [posts, setPosts] = useState(null);
-	const [error, setError] = useState('');
-	const [isLoading, setIsLoading] = useState(true);
-	useEffect(() => {
-		fetch(API_URL)
-			.then((response) => response.json())
-			.then((json) => setPosts(json))
-			.catch((error) => setError(error.message))
-			.finally(() => setIsLoading(false));
-	}, []);
+    const [posts, setPosts] = useState(null);
+    const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(as() => {
+            try {
+                const res = await fetch(API_URL);
+                const posts = await res.json();
+                setPosts(posts);
+            } catch (error) {
+                setError(error.message);
+            }
+            setIsLoading(false);
+        });
+}
+	// useEffect(() => {
+	// 	fetch(API_URL)
+	// 		.then((response) => response.json())
+	// 		.then((json) => setPosts(json))
+	// 		.catch((error) => setError(error.message))
+	// 		.finally(() => setIsLoading(false));
+	// }, []);
 
 	if (error) {
 		return <p>Error: {error}</p>;
