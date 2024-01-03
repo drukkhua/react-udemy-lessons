ee3a7affc8f6eb00a44646e6d65bb6f59c6f2898
Pretcb-Air <director@druk.kh.ua>
2024-01-03 22:33:51 +0200

    Error + loading add to Posts component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,13 @@
+import './Post.css';
+
 function Post(props) {
 	const { id, userId, title, body } = props;
 	return (
-		<div className="cards">
+		<div className="post">
 			<small>{id}</small>
 			<h2>{title}</h2>
 			<p>{body}</p>
-			<h3>userId: {userId}</h3>
+			<h3>User id: {userId}</h3>
 		</div>
 	);
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -4,18 +4,27 @@ import { useState } from 'react';
 
 function Posts() {
 	const [posts, setPosts] = useState(null);
+	const [error, setError] = useState('');
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/posts/')
 			.then((response) => response.json())
 			.then((json) => setPosts(json))
-			.catch((error) => console.log(error.message));
+			.catch((error) => setError(error.message))
+			.finally(() => setIsLoading(false));
 	}, []);
 
-	// console.log(posts);
-
+	if (error) {
+		return <p>Error: {error}</p>;
+	}
 	return (
 		<div>
-			{posts &&
+			<h1>Posts</h1>
+			<hr />
+			{isLoading ? (
+				<h1>Loading</h1>
+			) : (
+				posts &&
 				posts.map((post) => {
 					return (
 						<Post
@@ -23,7 +32,8 @@ function Posts() {
 							key={post.id}
 						/>
 					);
-				})}
+				})
+			)}
 		</div>
 	);
 }
